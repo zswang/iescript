563309851449558f6427357d0eb515900371dfa9
jihu.wang <jihu.wang@359507b6-eeb7-11de-b599-4d5b25800cad>
2011-03-02 17:08:04 +0000

    添加快捷键和用例

---
+++ bin/云工具/云编码.js
@@ -0,0 +1,5 @@
+(function () {
+    var e = document.createElement('script');
+    e.setAttribute('src', 'http://csdntools.googlecode.com/svn/trunk/base64/index.js');
+    document.body.appendChild(e);
+})();
---
+++ bin/云工具/搜狗输入法.js
@@ -0,0 +1,5 @@
+(function () {
+    var e = document.createElement('script');
+    e.setAttribute('src', 'http://web.pinyin.sogou.com/web_ime/init.js');
+    document.body.appendChild(e);
+})();
